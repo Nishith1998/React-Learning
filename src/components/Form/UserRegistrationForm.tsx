c694947b54e2de12302f5463112b39a082fe118c
Nishith1998 <nishithgoswami23@gmail.com>
2023-07-05 09:22:25 +0530

    Edit try

--- src/components/Form/UserRegistrationForm.tsx
+++ src/components/Form/UserRegistrationForm.tsx
@@ -1,7 +1,7 @@
 import { Card } from "../UI/Card/Card";
 import { GenericForm } from "../UI/GenericForm/GenericForm";
-import { FORM_FIELDS } from "../../models/constants";
-import { GenericFormField, FormValue } from "../../models/types";
+import { FORM_FIELDS, FROM_STRUCTURE_INITIAL } from "../../models/constants";
+import { GenericFormField, FormValue, FormStructure } from "../../models/types";
 import { useEffect } from "react";
 
 type UserRegistrationFormProps = {
@@ -18,17 +18,19 @@ export const UserRegistrationForm = (props: UserRegistrationFormProps) => {
     });
   };
 
-  useEffect(() => {}, [props.formValue]);
+  let genericFormValue: FormStructure<FormValue> = FROM_STRUCTURE_INITIAL; 
+  useEffect(() => {
+    FORM_FIELDS.forEach((fields: GenericFormField<FormValue>) => {
+      genericFormValue[fields.id] = { value: props.formValue[fields.id], isValid: true, isTouched: true }
+    });
   
-  // const formFields = FORM_FIELDS.map((fields: GenericFormField<FormValue>) => {
-  //   fields.value = props.formValue[fields.id];
-  //   return fields;
-  // });
+  }, [props.formValue, genericFormValue]);
+  console.log("Form: formValue: ",props.formValue)
 
   return (
     <Card className="flex-col bg-slate-50">
       <div className="text-lg text-center w-full">Registration Form</div>
-      <GenericForm formFields={FORM_FIELDS} onSubmit={onSubmit} />
+      <GenericForm formFields={[...FORM_FIELDS]} genericFormValue={genericFormValue} onSubmit={onSubmit} />
     </Card>
   );
 };
